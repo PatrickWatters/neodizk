78f088e5fae125bf02fa67ada16f9935e54d4911
patrickwatters <PATRICKWATTERS33@GMAIL.COM>
2022-09-09 11:54:05 +0200

    run script update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,32 @@
 /root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 10 1 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 10 2 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 10 4 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 10 8 2G
+
 /root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 11 1 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 11 2 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 11 4 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 11 8 2G
+
 /root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 12 1 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 12 2 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 12 4 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 12 8 2G
+
 /root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 13 1 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 13 2 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 13 4 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 13 8 2G
+
 /root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 14 1 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 14 2 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 14 4 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 14 8 2G
+
 /root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 15 1 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 15 2 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 15 4 2G
+/root/spark-3.3.0-bin-hadoop3/bin/spark-submit --class profiler.Profiler --master spark://cluster-master:7077 /home/dizk/target/neodizk-0.2.0.jar "zksnark-large" 15 8 2G
+
+
 cp /tmp/spark-events/src/main/resources/logs/ETE-SNARK-Logging.tsv /home/dizk/ETE-SNARK-Logging.tsv
